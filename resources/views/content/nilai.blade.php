2cfbf478dd291b81fb3a21fa83ba1ef7b73b012a
varid68 <varid68@gmil.com>
2018-02-04 17:32:16 +0700

    show list of students scores

--- resources/views/content/nilai.blade.php
+++ resources/views/content/nilai.blade.php
@@ -5,7 +5,45 @@
 @endsection
 
 @section('content')
-  <h2>Hai..</h2>
+  <div class="box">
+    <div class="box-header">
+      <h3 class="box-title">Condensed Full Width Table</h3>
+    </div>
+    <!-- /.box-header -->
+    <div class="box-body no-padding">
+      <table class="table table-condensed">
+        <tr>
+          <th style="width: 3%">#</th>
+          <th style="width: 12%">NIM</th>
+          <th style="width: 25%">Nama</th>
+          <th style="width: 10%">Absensi</th>
+          <th style="width: 10%">Tugas</th>
+          <th style="width: 10%">Uts</th>
+          <th style="width: 10%">Uas</th>
+          <th style="width: 10%">Nilai akhir</th>
+          <th style="width: 10%">Option</th>
+        </tr>
+        @php $no = 1 @endphp
+        @foreach((array) $list as $item)
+        <tr>
+          <td>{{ $no++ }}</td>
+          <td>{{ $item["nim"] }}</td>
+          <td>{{ $item["nama"] }}</td>
+          <td>{{ $item["absensi"] }}</td>
+          <td>{{ $item["tugas"] }}</td>
+          <td>{{ $item["uts"] }}</td>
+          <td>{{ $item["uas"] }}</td>
+          <td>{{ $item["nilai_akhir"] }} <span style="color:#00a65a;font-weight:500">({{ $item["grade"] }})</span></td>
+          <td>
+            <button type="button" class="btn btn-sm btn-success btn-modal" data-alamat="" 
+              data-ta="" data-toggle="modal" data-target="#modal-detail">Detail</button>
+          </td>
+        </tr>
+        @endforeach
+      </table>
+    </div>
+    <!-- /.box-body -->
+  </div>
 @endsection
 @section('script')
 @endsection
