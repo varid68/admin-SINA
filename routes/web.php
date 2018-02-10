38901ba679a94bd1f56627458eea194898c44c61
varid68 <varid68@gmil.com>
2018-02-10 07:57:49 +0700

    combine all middleware route into group & naming all route

--- routes/web.php
+++ routes/web.php
@@ -14,30 +14,32 @@
 Route::get('/', function () {
   return view('login');
 });
-
 Route::post('/login', 'LoginController@index')->name('login');
 Route::get('/login/{id}', 'LoginController@setSession')->name('set-sesi');
 Route::get('/logout', 'LoginController@logout')->name('logout');
 
-Route::get('/home', 'HomeController@index')->name('home')->middleware('key');
+Route::middleware('key')->group(function() {
+  Route::get('/home', 'HomeController@index')->name('home');
+  
+  Route::get('/news/{page?}', 'NewsController@index')->name('news list');
+  Route::post('/news/input', 'NewsController@store')->name('input new news');
+  Route::delete('/news/{id}', 'NewsController@delete')->name('delete news');
 
-Route::get('/nilai', 'NilaiController@index')->name('point')->middleware('key');
-Route::get('/nilai-pdf', 'NilaiController@downloadPdf')->name('point')->middleware('key');
+  Route::get('/mahasiswa', 'MahasiswaController@index')->name('list all students');
+  Route::get('/mahasiswa-pdf', 'MahasiswaController@downloadPdf')->name('download students list');
+  Route::get('/mahasiswa/datatable', 'MahasiswaController@datatable')->name('ajax datatable');
+  
+  Route::get('/alumni/{page?}', 'AlumniController@index')->name('list all alumni');
+  Route::get('/alumni-pdf', 'AlumniController@downloadPdf')->name('download alumni list');
+  
+  Route::get('/nilai', 'NilaiController@index')->name('list of students score');
+  Route::get('/nilai-pdf', 'NilaiController@downloadPdf')->name('download students score');
 
-Route::get('/input-nilai/{page?}', 'InputNilaiController@index')->middleware('key');
-Route::post('/input-nilai', 'InputNilaiController@store')->middleware('key');
+  Route::get('/input-nilai/{page?}', 'InputNilaiController@index')->name('show view');
+  Route::post('/input-nilai', 'InputNilaiController@store')->name('insert students score');
 
-Route::get('/mahasiswa', 'MahasiswaController@index')->middleware('key');
-Route::get('/mahasiswa-pdf', 'MahasiswaController@downloadPdf')->middleware('key');
-Route::get('/mahasiswa/datatable', 'MahasiswaController@datatable')->middleware('key');
-
-Route::get('/alumni/{page?}', 'AlumniController@index')->middleware('key');
-Route::get('/alumni-pdf', 'AlumniController@downloadPdf')->middleware('key');
-
-Route::get('/remidial', 'RemidialController@index')->middleware('key');
-Route::get('/remidial/edit', 'RemidialController@edit')->middleware('key');
-Route::get('/remidial-pdf', 'RemidialController@downloadPdf')->middleware('key');
-
-Route::get('/news/{page?}', 'NewsController@index')->middleware('key');
-Route::post('/news/input', 'NewsController@store')->middleware('key');
-Route::delete('/news/{id}', 'NewsController@delete')->middleware('key');
+  Route::get('/remidial', 'RemidialController@index')->name('list remedial');
+  Route::get('/remidial/edit', 'RemidialController@edit')->name('edit remedial score');
+  Route::get('/remidial-pdf', 'RemidialController@downloadPdf')->name('download remedial score');
+  
+});
